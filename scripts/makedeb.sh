aed8d50d2238090ac64f8c20cceef4f3d0438fd8
Fabian Baena <fabbaena@gmail.com>
2023-02-07 13:56:08 -0500

    fix bugs

--- scripts/makedeb.sh
+++ scripts/makedeb.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+set -ex
+
+NP_PATH=$1
+
+if [ -z "${NP_PATH}" ]; then
+    echo "Please provide nagios-plugins source code path" >&2
+    exit 1
+fi
+
+if [ ! -d ${NP_PATH} ]; then
+    echo "NP_PATH ${NP_PATH} is not a directory." >&2
+    exit 1
+fi
+
 make dist
 CURDIR=$(pwd)
-BUILDDEV=$(mkdir -d)
+BUILDDEV=$(mktemp -d)
 cp samana-check-winrm-1.0.tar.gz ${BUILDDEV}
 cd ${BUILDDEV}
 tar -xzvf samana-check-winrm-1.0.tar.gz
@@ -11,7 +25,7 @@ debmake
 cp scripts/configuressl.sh debian/postinst
 TAB=$(printf "\t")
 cat <<EOF >> debian/rules
-export NP_PATH=../../nagios-plugins
+export NP_PATH=${NP_PATH}
 override_dh_usrlocal:
 override_dh_auto_configure:
 ${TAB}dh_auto_configure -- --prefix=/usr/local/nagios
