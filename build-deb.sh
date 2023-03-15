0f06a119b0ce155a2eb234c8210874c2936a3b65
Fabian Baena <fabbaena@gmail.com>
2023-03-15 10:10:18 -0400

    added build script

---
+++ build-deb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. /etc/os-release
+
+./configure --with-nagios-plugins=${NP_PATH} \
+    --prefix=/usr/local/nagios \
+    --sysconfdir=/usr/local/nagios/etc \
+    --libexecdir=/usr/local/nagios/libexec
+make install-groups-users
+make deb
+mv *.deb /usr/src/${VERSION_CODENAME}/
