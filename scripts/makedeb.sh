7377fb4e391f9d1507fc06be11ba731b2e7a8cdd
Fabian Baena <fabbaena@gmail.com>
2023-03-27 16:33:08 -0400

    add dependency to check-samana

--- scripts/makedeb.sh
+++ scripts/makedeb.sh
@@ -36,7 +36,7 @@ override_dh_usrlocal:
 override_dh_auto_configure:
 ${TAB}dh_auto_configure -- --prefix=/usr/local/nagios --sysconfdir=/usr/local/nagios/etc --libexecdir=/usr/local/nagios/libexec
 EOF
-sed -i -e "s/^\(Depends.*\)/\1, gss-ntlmssp (>= 0.7.0)/" debian/control
+sed -i -e "s/^\(Depends.*\)/\1, gss-ntlmssp (>= 0.7.0), samm-check-samana/" debian/control
 debuild
 cp ${BUILDDEV}/${DISTNAME}-1_amd64.deb ${CURDIR}
 rm -Rf ${BUILDDEV}
